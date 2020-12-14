03879b04ca6c8a441abf131916c5e40833d4802b
Xavier Salazar <salazar.xavier26@gmail.com>
2020-12-14 16:55:16 -0600

    styling landing screen

--- src/App.js
+++ src/App.js
@@ -1,90 +1,21 @@
 import React from "react";
-import Particles from "react-tsparticles";
+import Container from "react-bulma-components/lib/components/container";
+import { ParticlesBackground } from "./components/ParticlesBackground";
+import "./App.scss";
 
 export const App = () => (
   <>
-    <Particles
-      id="tsparticles"
-      height="100vh"
-      width="100%"
-      options={{
-        background: {
-          color: {
-            value: "#0e1212",
-          },
-        },
-        fpsLimit: 60,
-        interactivity: {
-          detectsOn: "canvas",
-          events: {
-            onClick: {
-              enable: false,
-              mode: "push",
-            },
-            onHover: {
-              enable: false,
-              mode: "repulse",
-            },
-            resize: true,
-          },
-          modes: {
-            bubble: {
-              distance: 400,
-              duration: 2,
-              opacity: 0.8,
-              size: 40,
-            },
-            push: {
-              quantity: 4,
-            },
-            repulse: {
-              distance: 200,
-              duration: 0.4,
-            },
-          },
-        },
-        particles: {
-          color: {
-            value: "#ffffff",
-          },
-          links: {
-            color: "#ffffff",
-            distance: 150,
-            enable: true,
-            opacity: 0.25,
-            width: 1,
-          },
-          collisions: {
-            enable: true,
-          },
-          move: {
-            direction: "random",
-            enable: true,
-            outMode: "bounce",
-            random: true,
-            speed: 2,
-            straight: false,
-          },
-          number: {
-            density: {
-              enable: true,
-              value_area: 950,
-            },
-            value: 90,
-          },
-          opacity: {
-            value: 0.25,
-          },
-          shape: {
-            type: "line",
-          },
-          size: {
-            random: true,
-            value: 3,
-          },
-        },
-        detectRetina: true,
-      }}
-    />
+    <ParticlesBackground />
+    <Container className="nav">
+      <h4 className="nav-item">about me</h4>
+      <h4 className="nav-item">work</h4>
+      <h4 className="nav-item">contact</h4>
+    </Container>
+    <Container className="main-background" fluid>
+      <h1 className="name">Xavier Salazar</h1>
+      <p className="subtitle has-text-grey-lighter">
+        front-end, full-stack, and mobile developer for 7+ years
+      </p>
+    </Container>
   </>
 );
---
+++ src/components/ParticlesBackground.js
@@ -0,0 +1,93 @@
+import React from "react";
+import Particles from "react-tsparticles";
+
+export const ParticlesBackground = () => (
+  <Particles
+    id="tsparticles"
+    height="100vh"
+    width="100%"
+    style={{
+      position: "absolute",
+      top: 0,
+      left: 0,
+    }}
+    options={{
+      background: {
+        color: {
+          value: "#0e1212",
+        },
+      },
+      fpsLimit: 60,
+      interactivity: {
+        detectsOn: "canvas",
+        events: {
+          onClick: {
+            enable: false,
+            mode: "push",
+          },
+          onHover: {
+            enable: false,
+            mode: "repulse",
+          },
+          resize: true,
+        },
+        modes: {
+          bubble: {
+            distance: 400,
+            duration: 2,
+            opacity: 0.25,
+            size: 40,
+          },
+          push: {
+            quantity: 4,
+          },
+          repulse: {
+            distance: 200,
+            duration: 0.4,
+          },
+        },
+      },
+      particles: {
+        color: {
+          value: "#ffffff",
+        },
+        links: {
+          color: "#ffffff",
+          distance: 150,
+          enable: true,
+          opacity: 0.25,
+          width: 1,
+        },
+        collisions: {
+          enable: true,
+        },
+        move: {
+          direction: "random",
+          enable: true,
+          outMode: "bounce",
+          random: true,
+          speed: 2,
+          straight: false,
+        },
+        number: {
+          density: {
+            enable: true,
+            value_area: 750,
+          },
+          value: 70,
+        },
+        opacity: {
+          value: 0.25,
+        },
+        shape: {
+          type: "line",
+        },
+        size: {
+          random: true,
+          value: 3,
+        },
+      },
+      detectRetina: true,
+    }}
+  />
+);

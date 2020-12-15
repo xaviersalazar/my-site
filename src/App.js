14cc3afd1db5d6b56a633c7dff0503d11a67ed41
Xavier Salazar <salazar.xavier26@gmail.com>
2020-12-15 10:31:12 -0600

    more styling of landing page and navbar

--- src/App.js
+++ src/App.js
@@ -1,15 +1,21 @@
 import React from "react";
 import Container from "react-bulma-components/lib/components/container";
-import { ParticlesBackground } from "./components/ParticlesBackground";
+import { ParticlesBackground } from "./components/particles/ParticlesBackground";
 import "./App.scss";
 
 export const App = () => (
   <>
     <ParticlesBackground />
     <Container className="nav">
-      <h4 className="nav-item">about me</h4>
-      <h4 className="nav-item">work</h4>
-      <h4 className="nav-item">contact</h4>
+      <a href="#" className="nav-item">
+        about me
+      </a>
+      <a href="#" className="nav-item">
+        work
+      </a>
+      <a href="#" className="nav-item">
+        contact
+      </a>
     </Container>
     <Container className="main-background" fluid>
       <h1 className="name">Xavier Salazar</h1>

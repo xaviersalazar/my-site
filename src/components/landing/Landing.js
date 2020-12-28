2707880c500a88330257d6275313aecfd389dfd9
Xavier Salazar <salazar.xavier26@gmail.com>
2020-12-28 13:33:35 -0600

    removing bulma

--- src/components/landing/Landing.js
+++ src/components/landing/Landing.js
@@ -1,12 +1,11 @@
 import React from "react";
-import Container from "react-bulma-components/lib/components/container";
 import "./Landing.scss";
 
 export const Landing = () => (
-  <Container className="main-background">
+  <div className="main-background">
     <h1 className="name">Xavier Salazar</h1>
     <p className="subtitle has-text-grey-light">
       front-end, full-stack, and mobile developer
     </p>
-  </Container>
+  </div>
 );

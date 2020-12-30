ecb4c53cac2a31c1aa135a9f29d2f83d4da71ae0
Xavier Salazar <salazar.xavier26@gmail.com>
2020-12-30 15:46:05 -0600

    Updates

--- src/components/landing/Landing.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import "./Landing.scss";
-
-export const Landing = () => (
-  <div className="main-background">
-    <h1 className="name">Xavier Salazar</h1>
-    <p className="subtitle">front-end, full-stack, and mobile developer</p>
-  </div>
-);
--- src/components/nav/NavItem.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import "./Nav.scss";
-
-export const NavItem = ({ title, link }) => (
-  <a href={link} className="nav-item">
-    {title}
-  </a>
-);
